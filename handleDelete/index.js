0cd4432aaae5671cd92da76c0288abb0f0b56b47
Jonathan Tsai <modopo@gmail.com>
2023-04-26 17:35:54 -0700

    update and delete

--- handleDelete/index.js
+++ handleDelete/index.js
@@ -8,3 +8,23 @@ const schema = new dynamoose.Schema({
 })
 
 const employeeModel = dynamoose.model('Employee', schema);
+
+exports.handler = async (event) => {
+  let param = event.pathParameters;
+
+  const response = {
+    statusCode: 200,
+    body: null
+  };
+
+  if (param) {
+    let deleted = await employeeModel.delete(param['id']);
+    response.body = JSON.stringify(deleted);
+    response.statusCode = 200;
+  } else {
+    response.body = JSON.stringify("Not available");
+    response.statusCode = 500;
+  }
+
+  return response;
+}
